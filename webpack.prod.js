9437b32bee5021b497a2c478b61ff0aee3b498c0
Мухомедзянов Данил <danilm@sibguti.ru>
2025-08-07 16:41:03 +0700

    Add ghpages

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: CC0-1.0
 
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -11,6 +12,12 @@ module.exports = {
     filename: 'webcomp-boilerplate.min.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html', // Path to your template HTML file
+      filename: 'index.html' // Output filename
+    })
+  ],
   module: {
     rules: [
       {
